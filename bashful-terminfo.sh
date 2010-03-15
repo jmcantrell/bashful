0fcdb71fd081debac01e3f2127eea625122b122a
Jeremy Michael Cantrell <jmcantrell@gmail.com>
2010-03-15 03:26:32 -0400

    Bug fixes, more colors supported, efficiency improvements

--- bashful-terminfo.sh
+++ bashful-terminfo.sh
@@ -3,12 +3,12 @@
 # Filename:      bashful-terminfo.sh
 # Description:   Sets terminal strings for things like color, bold, etc.
 # Maintainer:    Jeremy Cantrell <jmcantrell@gmail.com>
-# Last Modified: Mon 2010-03-01 00:15:27 (-0500)
+# Last Modified: Mon 2010-03-15 03:24:00 (-0400)
 
 # doc bashful-terminfo {{{
 #
 # The terminfo library provides variables for altering the appearance of
-# terminal output (bold, italics, underline, colors, etc).
+# terminal output (bold, standout, underline, colors, etc).
 #
 # doc-end bashful-terminfo }}}
 
@@ -20,31 +20,52 @@ fi
 
 [[ $BASHFUL_TERMINFO_LOADED ]] && return
 
-term_reset=$(tput sgr0 2>/dev/null)
-term_bold=$(tput bold 2>/dev/null)
-term_italics=$(tput sitm 2>/dev/null)
-term_underline=$(tput smul 2>/dev/null)
-term_blink=$(tput blink 2>/dev/null)
+term_colors=$(tput colors)
 
-if (( $(tput colors) >= 8 )); then
+P() { echo "${1}${2}${term_reset}"; }
+
+C()
+{
+    local name="term_${1}g_$2"
+    if [[ ! ${!name} ]] && (( $term_colors >= 8 )); then
+        eval "$name=\"$(tput seta$1 $2)\""
+    fi
+    echo "${!name}${2}${term_reset}"
+}
+
+FG() { C f "$1" "$2"; }
+BG() { C b "$1" "$2"; }
+
+FX() { P "$(named "term_$1")" "$2"; }
+
+term_reset=$(tput sgr0)
+term_bold=$(tput bold)
+term_dim=$(tput dim)
+term_standout=$(tput smso)
+term_italic=$(tput sitm)
+term_underline=$(tput smul)
+term_blink=$(tput blink)
+term_reverse=$(tput rev)
+
+if (( $term_colors >= 8 )); then
     # Foreground colors
-    term_fg_black=$(tput setaf 0 2>/dev/null)
-    term_fg_red=$(tput setaf 1 2>/dev/null)
-    term_fg_green=$(tput setaf 2 2>/dev/null)
-    term_fg_yellow=$(tput setaf 3 2>/dev/null)
-    term_fg_blue=$(tput setaf 4 2>/dev/null)
-    term_fg_magenta=$(tput setaf 5 2>/dev/null)
-    term_fg_cyan=$(tput setaf 6 2>/dev/null)
-    term_fg_white=$(tput setaf 7 2>/dev/null)
+    term_fg_black=$(tput setaf 0)
+    term_fg_red=$(tput setaf 1)
+    term_fg_green=$(tput setaf 2)
+    term_fg_yellow=$(tput setaf 3)
+    term_fg_blue=$(tput setaf 4)
+    term_fg_magenta=$(tput setaf 5)
+    term_fg_cyan=$(tput setaf 6)
+    term_fg_white=$(tput setaf 7)
     # Background colors
-    term_bg_black=$(tput setbf 0 2>/dev/null)
-    term_bg_red=$(tput setbf 1 2>/dev/null)
-    term_bg_green=$(tput setbf 2 2>/dev/null)
-    term_bg_yellow=$(tput setbf 3 2>/dev/null)
-    term_bg_blue=$(tput setbf 4 2>/dev/null)
-    term_bg_magenta=$(tput setbf 5 2>/dev/null)
-    term_bg_cyan=$(tput setbf 6 2>/dev/null)
-    term_bg_white=$(tput setbf 7 2>/dev/null)
+    term_bg_black=$(tput setab 0)
+    term_bg_red=$(tput setab 1)
+    term_bg_green=$(tput setab 2)
+    term_bg_yellow=$(tput setab 3)
+    term_bg_blue=$(tput setab 4)
+    term_bg_magenta=$(tput setab 5)
+    term_bg_cyan=$(tput setab 6)
+    term_bg_white=$(tput setab 7)
 fi
 
 BASHFUL_TERMINFO_LOADED=1
